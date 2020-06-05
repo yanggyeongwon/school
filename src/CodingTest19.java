07e98544b96d8b96b1fae7dca6be2f35b1159fba
admin <admin@DESKTOP-HUR5EQG>
2020-06-05 18:21:35 +0900

    오늘 마지막에 한거

--- src/CodingTest19.java
+++ src/CodingTest19.java
@@ -3,17 +3,33 @@ import java.util.Scanner;
 public class CodingTest19 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		
-		if(N%2==0) {
-			System.out.print("*");
-			for(int i = 0;i<N; i++) {
-				System.out.print(" *");
-				
+		Scanner in = new Scanner(System.in);
+		 
+		int N = in.nextInt();
+		in.close();
+ 
+		for(int i = 1; i <= 2*N; i++){//N이 2일때 1<=4
+			if(i%2 == 1){//2배수 1로 떨어지는애들
+				for(int j = 1; j <= N; j++){
+					if(j%2 == 1){
+						System.out.print("*1");
+					}
+					else{
+						System.out.print(" 1");
+					}
+				}
 			}
+			else{//2배수가 1로 안떨어지는 아닌 애들
+				for(int j = 1; j <= N; j++){
+					if(j%2 == 1){
+						System.out.print(" 2");
+					}
+					else{
+						System.out.print("*2");
+					}
+				}
+			}
+			System.out.print("\n");
 		}
-		
 	}
-
 }
--- src/Ex01.java
+++ src/Ex01.java
@@ -1,20 +1,7 @@
 
-import java.util.Scanner;
-
 public class Ex01 {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		
-		//System.out.println(a+" "+b);
-		System.out.printf("a+b=%d\n",a+b);
-		System.out.printf("a-b=%d\n",a-b);
-		System.out.printf("a*b=%d\n",a*b);
-		System.out.printf("a/b=%.2f",(float)a/b);
-	}
-
+    public static void main(String[] args) {
+       
+    }
 }
