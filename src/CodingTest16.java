9afe1c942cb13c5926dd0da4ebd21c5e8ed00c21
admin <admin@DESKTOP-HUR5EQG>
2020-06-05 15:05:59 +0900

    구구단 배수 까지

---
+++ src/CodingTest16.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CodingTest16 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int [] array = new int[3];
+		
+		for(int i = 0; i<array.length;i++) {
+			array[i] = sc.nextInt();
+		}
+		Arrays.sort(array);
+		System.out.println(array[1]);
+		
+
+	}
+
+}
--- src/Ex03.java
+++ src/Ex03.java
@@ -2,85 +2,30 @@
 public class Ex03 {
 
 	public static void main(String[] args) {
-//		-----------------------------------------------
-//		 int i = 1;
-//		 switch (i) {
-//		 	case 1:
-//		 		System.out.println("A");
-//		 		break;
-//
-//		 	default:
-//		 		System.out.println("default");
-//		}
 		
-//		int i = 11;
-//		int j = (i%2==0) ? 0 : 1;
-//		
-//		System.out.println("j¿« ∞™ : "+j);	
-//		-----------------------------------------------
-//		if(j != 0) {
-//			
-//			System.out.println("»¶");
-//			System.out.println("if∞™¿Ã ture¿œ∂ß "+j);	
-//		}else {
-//			System.out.println("¬¶");
-//			System.out.println("if∞™¿Ã false¿œ∂ß "+j);	
+//		for(int i=1; i<=5; i++) {
+//			for(int j=0; j<i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.print("\n");
 //		}
-//		-----------------------------------------------
-//		switch (j) {
-//		case 1:
-//			System.out.println("»¶");	
-//			System.out.println("case 1: "+j);	
-//			break;
-//
-//		default:
-//			System.out.println("¬¶");
-//			System.out.println("default :"+j);	
+//		-----------------------------∞˙¡¶
+//		for(int i=5; i>0; i--) {
+//			for(int j=0; j<i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.print("\n");
 //		}
-//		-----------------------------------------------
-//		int Q = 50;
-//		int W = 40;
-//		int E = 30;
-//		int R = 50;
-//		//int sum = ((Q+W+E+R)/4)>40 ? 1 : 2;
-//		int sum = Q+W+E+R;
-//		
-//		System.out.println(sum);
-//		-----------------------------------------------
-//		switch (sum/4) {
-//		case 40:
-//			System.out.println("40∆Ú±’¿ÃªÛ");
-//			break;
-//		case 42:
-//			System.out.println("42¿ÃªÛ");
-//			break;
-//		default: 
-//			System.out.println("40∆Ú±’¿Ã«œ");
-//		}
-//		-----------------------------------------------
-//		StringBuilder sc = new StringBuilder();
-//		
-//		sc.append("fruit");
-//		System.out.println("fruit : "+sc.toString());
-//		------------------------------------------
-//		int sum=0;
-//		for(int i=1; i<=10 ; i++) {
-//			
-//			sum += i;
-//			
-//		}
-//		System.out.println(sum);
-//		-----------------------------------------------
-		int sum = 0,sum2 = 0;
-		for(int i=1; i<=10 ; i++) {
-			if(i%2==0) {
-				sum += i;
-			}else {
-				sum2 += i;
+//		----------------------------∞˙¡¶
+		
+		for(int i=2; i<10; i++) {
+			if(i%2==1) {
+				continue;
+			}
+			for(int j=1; j<10; j++) {
+				if(j>i) break;
+				System.out.println(i+"*"+j+"="+i*j);
 			}
 		}
-		System.out.println(sum);
-		System.out.println(sum2);
 	}
-
 }
