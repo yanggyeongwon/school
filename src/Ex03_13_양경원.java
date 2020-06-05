9a341886a8808b46caaff9585f5d9d061d54bffe
admin <admin@DESKTOP-HUR5EQG>
2020-06-05 17:16:56 +0900

    ++추가 while

--- src/Ex03_13_양경원.java
+++ src/Ex03_13_양경원.java
@@ -5,12 +5,35 @@ public class Ex03_13_양경원 {
 		
 		
 		int sum = 0;
+		
 		for(int i = 1; i<=100; i++) {
-			sum +=i;
+			sum += i;
 			if(i%10 == 0) {
 				System.out.println(sum);
 			}
 		}	
+		System.out.println("-------------------------절취선-------------------------");
+		int sum1 = 0;
+		
+		for(int j = 1; j<=100; j++) {
+			sum1 += j;
+			if((j%10) == 0) {
+				System.out.println(sum1);
+				sum1 = 0;
+			}
+		}
+		System.out.println("-------------------------절취선-------------------------");
+		int K=1;
+		int L=0;
+		while(K <= 100) {
+			L += K;
+			
+			if(K%10 == 0) {
+				System.out.println(L);
+			}
+			K++;
+			
+		}
 	}
 	
 }
