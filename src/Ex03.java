ca81bc52fb1f76c093564013960605fff64a82d2
admin <admin@DESKTOP-HUR5EQG>
2020-06-08 18:09:20 +0900

    최종 20200608

--- src/Ex03.java
+++ src/Ex03.java
@@ -11,12 +11,12 @@ public class Ex03 {
 //			System.out.print("\n");
 //		}
 //		-----------------------------과제
-		for(int i=5; i>0; i--) {
-			for(int j=0; j<i; j++) {
-				System.out.print("*");
-			}
-			System.out.print("\n");
-		}
+//		for(int i=5; i>0; i--) {
+//			for(int j=0; j<i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.print("\n");
+//		}
 //		----------------------------과제
 		
 //		for(int i=2; i<10; i++) {
