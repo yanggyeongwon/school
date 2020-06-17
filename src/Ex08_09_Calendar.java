8294a2f4d3b6bc99dc8b1c8b4ba11f3673400f64
admin <admin@DESKTOP-HUR5EQG>
2020-06-17 12:38:02 +0900

    Calendar

--- src/Ex08_09_Calendar.java
+++ src/Ex08_09_Calendar.java
@@ -1,9 +1,19 @@
+import java.util.Calendar;
 
 public class Ex08_09_Calendar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//		Calendar c = new Calendar();protected 사용 x 같은패키지 다 사용/ 다른패키지 자식만 사용
+		Calendar c = Calendar.getInstance();
+		System.out.print("오늘날짜 : "+c.get(Calendar.YEAR)+"년 ");
+		System.out.print(c.get(Calendar.MONTH)+1+"월");//0부터 시작
+		System.out.print(c.get(Calendar.DATE)+"일 ");
+		
+		System.out.println(c.get(Calendar.DAY_OF_WEEK)+"요일");//1부터 시작
+		
+		String week [] = {"","일","월","화","수","목","금","토"};
+		
+		System.out.println(week[c.get(Calendar.DAY_OF_WEEK)]+"요일");
 	}
 
 }
