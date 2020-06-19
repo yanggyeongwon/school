a0f1248c4b8adae1ea04d6e26864dbad6f92a01b
ruddnjs25 <ruddnjs25@gmail.com>
2020-06-19 15:00:14 +0900

    Thread start run sleep runnable

---
+++ src/CodingTest/CodingTest26.java
@@ -0,0 +1,32 @@
+package CodingTest;
+
+import java.util.Scanner;
+
+public class CodingTest26 {
+
+	public static void main(String[] args) {
+		
+		 Scanner sc = new Scanner(System.in);
+	        int N = sc.nextInt();
+	        String[] cases = new String[N];
+	        for (int i = 0; i < N; i++) {
+	            cases[i] = sc.next();
+	        }
+	        sc.close();
+	         
+	        int count, total;
+	        for (String OXresult : cases) {
+	            count = 0;
+	            total = 0;
+	            for (int i = 0; i < OXresult.length(); ++i) {
+	                if (OXresult.charAt(i) == 'O') {
+	                    total += ++count;
+	                } else {
+	                    count = 0;
+	                }
+	            }
+	            System.out.println(total);
+	        }
+	}
+
+}
---
+++ src/Ex10_01_Thread.java
@@ -0,0 +1,58 @@
+class All {
+	
+}
+
+class Big extends Thread  {
+	public void run() {
+		//if (target != null) {
+		//        target.run();
+		//}
+		System.out.println("big()");
+		for(char i='A'; i<'Z';i++) {// Thread.interrupt() 어떠한 대상 인터럽트
+			System.out.print(i);
+			try {
+				sleep(10);
+			} catch (InterruptedException ex) {
+				// 간단한 테스팅 코드나 장난감 코드가 아니라면 이렇게 하지 말자!
+				// InterruptedException은 적절히 처리되어야 한다.
+			}
+
+		}
+		System.out.println();
+	}
+}
+class Small implements Runnable{
+	public void run() {
+		
+		for(char i='a'; i<'z';i++) {// Thread.interrupt() 어떠한 대상 인터럽트
+			System.out.print(i);
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException ex) {
+				// 간단한 테스팅 코드나 장난감 코드가 아니라면 이렇게 하지 말자!
+				// InterruptedException은 적절히 처리되어야 한다.
+			}
+		}
+		System.out.println();
+	}
+}
+
+public class Ex10_01_Thread {
+
+	public static void main(String[] args) {
+		System.out.println("프로그램시작");
+		Big b = new Big();
+		
+		b.start();
+		
+		Small c = new Small();
+		Thread th = new Thread(c);
+		th.start();
+		
+		System.out.println("프로그램종료");
+
+		
+
+	}
+
+}
